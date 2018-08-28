0e70b9d24f840e210ca2846af1879a4ad9132343
AJLoveChina <735267452@qq.com>
2018-08-28 20:52:36 +0800

    自动生成二维码

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,7 @@
 $(function () {
     let zhuziList = [];
     let zzW = 50,
-        zzH = 80;
+        zzH = 120;
     let zzOffsetW = 30,
         zzOffsetH = 20;
     let container = $(".container");
@@ -60,51 +60,71 @@ $(function () {
         item = $(item);
         let zhuziW = item.width(),
             zhuziH = item.height();
-        let w = 8,
-            h = 8;
-        for (let i = 0; i < 5; i++) {
-            list.push({
-                top: zhuziH / 6 * (i + 1),
-                left: -w / 2,
-                width: w,
-                height: h
-            })
-        }
-
-        for (let i = 0; i < 5; i++) {
-            list.push({
-                top: zhuziH / 6 * (i + 1),
-                right: -w / 2,
-                width: w,
-                height: h
-            })
-        }
-
-        list.forEach((css, i) => {
+        let w = 15,
+            h = 15;
+        for (let i = 0; i < 10; i++) {
+            let m = i % 5;
             let child = $("<div>");
             child.addClass("interface");
-            child.css(css);
+            child.css({
+                top: zhuziH / 6 * (m + 1),
+                width: w,
+                height: h,
+                fontSize: '12px',
+                lineHeight: h + "px"
+            });
+            if (i < 5) {
+                child.css({
+                    left : -w / 2
+                })
+            } else {
+                child.css({
+                    right : -w / 2
+                })
+            }
+            child.html(10 - i);
             item.append(child);
-        })
+        }
+
     });
 
-    $('.zhuzi').click(function () {
-        let index = $(this).attr("index");
+    let bianhao = {
+        "1": 101968,
+        "2": 101970,
+        "3": 101864,
+        "4": 101863,
+        "5": 101975,
+        "6": 101971,
+        "7": 101978,
+        "8": 101949,
+        "9": 101973,
+        "10": 101979,
+        "11": 101948,
+        "12": 101865
+    };
 
-        if ($(this).hasClass('disabled')) {
+    $('.zhuzi .interface').click(function () {
+        let zhuzi = $(this).parents('.zhuzi');
+        let index = zhuzi.attr("index") + "";
+
+        if (zhuzi.hasClass('disabled')) {
             return layer.msg("这个充电桩还没搞好")
         }
 
         layer.open({
             type: 1,
-            title: `${index}号充电桩二维码`,
+            title: `${bianhao[index]} - ${$(this).html()}号充电桩二维码`,
             skin: 'layui-layer-demo', //样式类名
             closeBtn: 0, //不显示关闭按钮
             anim: 2,
             shadeClose: true, //开启遮罩关闭
-            content: `<div style="padding:20px;"><p style="font-size:12px;line-height:20px;">扫描之后不知道自己的电瓶车插头编号<br>可以查看讯飞的那个小程序知晓<br>步骤:科大优享=>发现=>个人中心=>使用记录=>端口</p><img src="imgs/${index}.jpg"></div>`
+            content: `<div style="padding:20px;"><p style="font-size:12px;line-height:20px;">扫描之后不知道自己的电瓶车插头编号<br>可以查看讯飞的那个小程序知晓<br>步骤:科大优享=>发现=>个人中心=>使用记录=>端口</p><div id="qrcode"></div></div></div>`
         });
-    })
+
+        $('#qrcode').qrcode(`http://www.washpayer.com/userLogin?l=${bianhao[index]}&chargeindex=` + $(this).html());
+    });
+
+
 
     $(".contact").click(function () {
         layer.msg("联系邮箱 : 735267452@qq.com", {
